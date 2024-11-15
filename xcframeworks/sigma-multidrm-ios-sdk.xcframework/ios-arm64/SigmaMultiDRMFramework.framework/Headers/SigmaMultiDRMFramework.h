562d01d1efafe3bf28ab39020a0de94befafaff7
Phúc LĐ <phucld@gviet.vn>
2024-11-15 16:46:11 +0700

    feat: update sdk

--- xcframeworks/sigma-multidrm-ios-sdk.xcframework/ios-arm64/SigmaMultiDRMFramework.framework/Headers/SigmaMultiDRMFramework.h
+++ xcframeworks/sigma-multidrm-ios-sdk.xcframework/ios-arm64/SigmaMultiDRMFramework.framework/Headers/SigmaMultiDRMFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SigmaMultiDRMFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <SigmaMultiDRMFramework/PublicHeader.h>
 
 
-#import <SigmaMultiDRMFramework/SigmaMultiDRM.h>
+#import "SigmaMultiDRM.h"
