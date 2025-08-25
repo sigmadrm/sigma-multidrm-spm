73d21e8da9a6be3871428137c0aae310920d1ab5
Phúc LĐ <phucld@gviet.vn>
2025-08-25 16:35:28 +0700

    fix: update package.swift support tvOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SigmaMultiDRM",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         .library(
             name: "SigmaMultiDRM",
