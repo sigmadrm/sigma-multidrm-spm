9be56e82caa2a7329790e127b7b0c12de2642189
Phúc LĐ <phucld@gviet.vn>
2025-08-25 17:12:47 +0700

    fix: merge v1.0.4

--- xcframeworks/sigma-multidrm-ios-sdk.xcframework/ios-arm64_i386_x86_64-simulator/SigmaMultiDRMFramework.framework/Headers/SigmaMultiDRM.h
+++ xcframeworks/sigma-multidrm-ios-sdk.xcframework/ios-arm64_i386_x86_64-simulator/SigmaMultiDRMFramework.framework/Headers/SigmaMultiDRM.h
@@ -9,17 +9,67 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVAssetResourceLoader.h>
 #import <AVFoundation/AVAsset.h>
+#import "SigmaMultiDRMDelegate.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SigmaMultiDRM : NSObject
-+(SigmaMultiDRM *)getInstance;
--(AVURLAsset *)assetWithUrl:(NSString *)url;
--(void)setUserId:(NSString *)userId;
--(void)setSessionId:(NSString *)sessionId;
--(void)setMerchant:(NSString *)merchant;
--(void)setAppId:(NSString *)appId;
--(void)setDebugMode:(BOOL)debug;
+
+/**
+ * Get the singleton instance of SigmaMultiDRM
+ *
+ * @return The shared SigmaMultiDRM instance
+ */
++ (SigmaMultiDRM *)getInstance;
+
+/**
+ * The delegate object that will receive license request callbacks
+ */
+@property(nonatomic, assign) id<SigmaMultiDRMDelegate> delegate;
+
+/**
+ * Create an AVURLAsset with DRM protection for the given URL
+ *
+ * @param url The URL of the content to be protected
+ * @return An AVURLAsset configured with DRM protection
+ */
+- (AVURLAsset *)assetWithUrl:(NSString *)url;
+
+/**
+ * Set the user identifier for license requests
+ *
+ * @param userId The unique identifier for the user
+ */
+- (void)setUserId:(NSString *)userId;
+
+/**
+ * Set the session identifier for license requests
+ *
+ * @param sessionId The unique identifier for the current session
+ */
+- (void)setSessionId:(NSString *)sessionId;
+
+/**
+ * Set the merchant identifier for license requests
+ *
+ * @param merchant The unique identifier for the merchant
+ */
+- (void)setMerchant:(NSString *)merchant;
+
+/**
+ * Set the application identifier for license requests
+ *
+ * @param appId The unique identifier for the application
+ */
+- (void)setAppId:(NSString *)appId;
+
+/**
+ * Enable or disable debug mode
+ *
+ * @param debug YES to enable debug mode (staging environment), NO for production
+ */
+- (void)setDebugMode:(BOOL)debug;
+
 @end
 
 NS_ASSUME_NONNULL_END
